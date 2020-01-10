29aedc976693fad65112f5453bf02f1e55885bb4
edualb <eduardoslafiq@gmail.com>
2020-01-10 11:58:33 -0300

    add new lib

--- SPMCocoa/Pods/Target Support Files/Pods-SPMCocoa/Pods-SPMCocoa-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SPMCocoa : NSObject
-@end
-@implementation PodsDummy_Pods_SPMCocoa
-@end
--- SPMCocoa/Pods/Target Support Files/libExample/libExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_libExample : NSObject
-@end
-@implementation PodsDummy_libExample
-@end
